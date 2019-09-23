c9351cd3f0e2f1ad4ae1e7728bc6a44bba359d36
杨俊宁 <1003719811@qq.com>
2019-09-23 15:41:52 +0800

    add lint & lint:fix

--- template/.electron-vue/webpack.main.config.js
+++ template/.electron-vue/webpack.main.config.js
@@ -17,19 +17,6 @@ let mainConfig = {
   ],
   module: {
     rules: [
-{{#if eslint}}
-      {
-        test: /\.(js)$/,
-        enforce: 'pre',
-        exclude: /node_modules/,
-        use: {
-          loader: 'eslint-loader',
-          options: {
-            formatter: require('eslint-friendly-formatter')
-          }
-        }
-      },
-{{/if}}
       {
         test: /\.js$/,
         use: 'babel-loader',
